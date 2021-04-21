28d5380ea5baba753cb3936c6481dd7b6f357cbc
Anibal Ortega Piedrahita <anibalortegap@gmail.com>
2021-04-20 22:55:02 -0500

    [ADD] configurations for loader HTML,CSS and SASS - [ADD] optimizations for CSS and Javascript

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <h1>React Webpack</h1>;
+  return (
+    <div>
+      <h1>React Webpack</h1>
+    </div>
+  );
 };
 
 export default App;
